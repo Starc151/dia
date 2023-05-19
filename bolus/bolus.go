59f0e94fb53b95f79cd445874dd7564e9b78024d
Aleksey Pashkov <dr.Starc@ya.ru>
2023-05-19 21:43:07 +0300

    add hypo

--- bolus/bolus.go
+++ bolus/bolus.go
@@ -1,6 +1,9 @@
 package bolus
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 const (
 	lowerGlucose float64 = 7.0
@@ -10,18 +13,26 @@ const (
 
 func Bolus() {
 	sensitivityCoeff, carbohydrateCoeff := getCoefficients()
-	(idealGlucose - glucose) / (sensitivityCoeff * carbohydrateCoeff)
 	glucose := getRes("Уровень глюкозы: ")
 	xe := getRes("XE: ")
-
+	bolus := 0.0
+	corectGlucose := math.Abs(glucose - idealGlucose)
+	corrctXe := corectGlucose / (sensitivityCoeff * carbohydrateCoeff)
+	
 	if glucose <= lowerGlucose - 1.0 {
-		fmt.Println("Рекомендуется перекусить на: ". )
+		if xe != 0.0 {
+			xe -= corrctXe
+			bolus += carbohydrateCoeff * xe
+			fmt.Printf("Рекомендуемый болюс: %.1f", bolus)
+		} else {
+			fmt.Println("Рекомендуется перекусить на: ", corrctXe, "XE")
+		}
 	}
 
-	bolus := (glucose - idealGlucose) / sensitivityCoeff
-	if xe != 0.0{
-		bolus += carbohydrateCoeff * xe
-	}
+	// bolus = corectGlucose / sensitivityCoeff
+	// if xe != 0.0{
+	// 	bolus += carbohydrateCoeff * xe
+	// }
 
-	fmt.Printf("Рекомендуемый болюс: %.1f", bolus)
+	// fmt.Printf("Рекомендуемый болюс: %.1f", bolus)
 }
