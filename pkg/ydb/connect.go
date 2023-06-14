5a1cb16c7307adb73a9b75d8c124c1e00eb6c070
Starc151 <dr.starc@ya.ru>
2023-06-14 13:44:24 +0300

    connect ydb

---
+++ pkg/ydb/connect.go
@@ -0,0 +1,26 @@
+package ydb
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/ydb-platform/ydb-go-sdk/v3"
+	yc "github.com/ydb-platform/ydb-go-yc"
+)
+
+func Connect() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	db, err := ydb.Open(ctx,
+		"grpcs://ydb.serverless.yandexcloud.net:2135/ru-central1/b1gvs9nokmiitnhv21jt/etn1rjbm3bjs0a8emhjd",
+		yc.WithServiceAccountKeyFileCredentials("pkg/ydb/token/starc.json"),
+	)
+	if err != nil {
+		fmt.Println("No connect")
+	} else {
+		fmt.Println("CONNECTED!")
+	}
+	// createTable(db, ctx)
+
+	defer db.Close(ctx)
+}
