a79f493a4cf913ba74e8e0c8fe86f0ca821da1cf
Starc151 <dr.Starc@ya.ru>
2023-07-22 18:12:31 +0300

    upd view bolus

--- pkg/fyne/show.go
+++ pkg/fyne/show.go
@@ -12,10 +12,11 @@ import (
 func Show() {
 	a := app.New()
 	w := a.NewWindow("BL")
-	// w.Resize(fyne.NewSize(200, 230))
-	// w.SetFixedSize(true)
+	w.Resize(fyne.NewSize(205, 325))
+	w.SetFixedSize(true)
 	icon, _ := fyne.LoadResourceFromPath("pkg/fyne/icon.png")
 	w.SetIcon(icon)
+	iconUpdBtn, _ := fyne.LoadResourceFromPath("pkg/fyne/iconUpdBtn.png")
 
 	glucoseText := widget.NewLabel("GL: ")
 	glucose := onlyNums()
@@ -24,24 +25,24 @@ func Show() {
 	bolus := widget.NewLabel("")
 
 	fullHistory := bl.GetFullHistory()
-	t := func () *fyne.Container {
+	getHistory := func() *fyne.Container {
 		fullHistory = bl.GetFullHistory()
 		history := container.NewVBox()
-			if len(fullHistory) == 0 {
-				history.Add(widget.NewLabel("NO RESULT"))
-			} else {
-				for k, v := range fullHistory {
-					if k > 1 {
-						break
-					}
-					day := bl.GetDayHistory(v)
-					dayW := getHistoryWidget(day)
-					history.Add(dayW)
+		if len(fullHistory) == 0 {
+			history.Add(widget.NewLabel("NO RESULT"))
+		} else {
+			for k, v := range fullHistory {
+				if k > 1 {
+					break
 				}
+				day := bl.GetDayHistory(v)
+				dayW := getHistoryWidget(day)
+				history.Add(dayW)
 			}
+		}
 		return history
 	}
-	history := t()
+	history := getHistory()
 	bolusBtnVisParam := true
 	bolusBtnVis := func() {}
 	getBolusBtn := widget.NewButton("Calculate bolus",
@@ -54,26 +55,32 @@ func Show() {
 			)
 			bolusBtnVis()
 			history.RemoveAll()
-			history.Add(t())
+			history.Add(getHistory())
 		},
 	)
-	updBtn := widget.NewButton("upd",
+	updBtn := widget.NewButton("",
 		func() {
 			bolusBtnVis()
 		},
 	)
+	getBolusBtn.Resize(fyne.NewSize(150, 30))
+	getBolusBtn.Move(fyne.NewPos(5, 5))
 	updBtn.Disable()
+	updBtn.Resize(fyne.NewSize(30, 30))
+	updBtn.Move(fyne.NewPos(160, 5))
 	bolusBtnVis = func() {
 		bolusBtnVisParam = !bolusBtnVisParam
 		if !bolusBtnVisParam {
 			getBolusBtn.Disable()
 			updBtn.Enable()
+			updBtn.SetIcon(iconUpdBtn)
 		} else {
 			getBolusBtn.Enable()
 			updBtn.Disable()
+			updBtn.SetIcon(nil)
 		}
 	}
-
+	btns := container.NewWithoutLayout(getBolusBtn, updBtn)
 	tabs := container.NewAppTabs(
 		container.NewTabItem(
 			"Bolus",
@@ -82,7 +89,7 @@ func Show() {
 					glucoseText, glucose,
 					xeText, xe,
 				),
-				container.NewGridWithColumns(2, getBolusBtn, updBtn),
+				btns,
 				bolus,
 			),
 		),
