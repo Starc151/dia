0ac4e5f7af84fcad3108095c558adcadc073cee3
Starc151 <dr.Starc@ya.ru>
2023-07-26 13:34:15 +0300

    fix

--- pkg/ydb/connect.go
+++ pkg/ydb/connect.go
@@ -9,10 +9,11 @@ import (
 
 func connect() (*ydb.Driver, context.Context, context.CancelFunc){
 	ctx, cancel := context.WithCancel(context.Background())
-	
+	// key := ""
 	db, _ := ydb.Open(ctx,
 		"grpcs://ydb.serverless.yandexcloud.net:2135/ru-central1/b1gvs9nokmiitnhv21jt/etn1rjbm3bjs0a8emhjd",
 		yc.WithServiceAccountKeyFileCredentials("token/starc.json"),
+		// yc.WithServiceAccountKeyCredentials(key),
 	)
 	// if err != nil {
 	// 	fmt.Println("No connect")
